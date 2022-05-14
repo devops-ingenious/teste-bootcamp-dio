0eebede08a239b0d062b0dbaeb77fd9d52a8f7e5
Jezimiel Marcondes <jpamarcondes@gmail.com>
2022-05-14 16:32:12 -0300

    Add class Livro

--- primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
+++ primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -8,10 +8,46 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
-
+        Livro livro = new Livro("A Bíblia", 2500);
+        System.out.println(livro);
 
 /*        String primeiroNome = "Maria";
         String segundoNome = "Cecília";
         System.out.println("Alô, " + primeiroNome + " " + segundoNome + "!");*/
     }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPages;
+
+    public Livro(String nome, Integer numPages) {
+        this.nome = nome;
+        this.numPages = numPages;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(Integer numPages) {
+        this.numPages = numPages;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPages=" + numPages +
+                '}';
+    }
+
 }

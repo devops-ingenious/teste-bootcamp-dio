b4cddba4fc4e1e0d10a226bf7f4caae8be2b37ad
Jezimiel Marcondes <jpamarcondes@gmail.com>
2022-05-14 16:25:45 -0300

    First commit

---
+++ primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+
+/*        String primeiroNome = "Maria";
+        String segundoNome = "Cecília";
+        System.out.println("Alô, " + primeiroNome + " " + segundoNome + "!");*/
+    }
+}
